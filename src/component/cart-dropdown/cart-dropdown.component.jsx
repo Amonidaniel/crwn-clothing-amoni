b200a2dcb22f16d8a72768daefc99e27ab79d0fe
Amonidaniel <danielamoni1410@gmail.com>
2022-12-24 11:18:05 -0500

    adding cartItem and CartCount functionality

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../context/cart.context';
+
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>GO TO CHECKOUT</Button>
+                {cartItems.map((item) => ( 
+                    <CartItem key={item.id} item={item} />))}
             </div>
+            <Button>GO TO CHECKOUT</Button>
 
         </div>
     );
