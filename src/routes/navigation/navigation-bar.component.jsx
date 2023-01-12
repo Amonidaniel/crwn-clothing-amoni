889d5a90322538ac217fb798e82b0312697afdb9
Amonidaniel <danielamoni1410@gmail.com>
2023-01-12 15:35:06 -0500

    adding routes to directory component

--- src/routes/navigation/navigation-bar.component.jsx
+++ src/routes/navigation/navigation-bar.component.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom'
 import { ReactComponent as CrwnLogo } from '../../assests/crown.svg'; 
-import './navigation.styles.scss'
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles';
 import { UserContext } from '../../component/context/user.context';
 import { CartContext } from '../../component/context/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
@@ -17,25 +17,25 @@ const NavigationBar = () => {
 
    return (
     <Fragment>
-        <div className='navigation'>
-            <Link className='logo-container' to='/'>
+        <NavigationContainer>
+            <LogoContainer to='/'>
                 <CrwnLogo className='logo' />
-            </Link>
-           <div className='nav-links-container'>
-                <Link className='nav-link' to='/shop'>
+            </LogoContainer>
+           <NavLinks>
+                <NavLink to='/shop'>
                     SHOP
-                </Link>
+                </NavLink>
                {
                 currentUser ? (
-                    <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
-                ) : (<Link className='nav-link' to='/auth'>
+                    <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
+                ) : (<NavLink to='/auth'>
                 SIGN IN
-            </Link>)
+            </NavLink>)
                }
                <CartIcon />
-           </div>
+           </NavLinks>
            {isCartOpen && <CartDropdown />}
-        </div>
+           </NavigationContainer>
         <Outlet />
      </Fragment>
 
